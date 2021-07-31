4c82b6fcca859bd236e558ebe652aa7ce1a3ec9b
saifoza6 <86571553+saifoza6@users.noreply.github.com>
2021-08-01 01:13:02 +0700

    Update change.sh

--- change.sh
+++ change.sh
@@ -2,16 +2,6 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl http://34.101.77.252:6969/server/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 echo -e ""| lolcat
 echo -e "======================================"| lolcat
